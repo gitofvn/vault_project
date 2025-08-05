a4040b8d3f46008b5be6c1a6e9b42e5d05153689
gitofvn <velin.nikolov.bn@gmail.com>
2025-08-05 13:08:35 +0300

    Add slight UI tweask, icons for credentials and success message after registration

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.contrib import messages
+
 
 from accounts.forms import RegisterUserForm
 
@@ -20,6 +22,10 @@ class RegisterUserView(CreateView):
     form_class = RegisterUserForm
     template_name = 'accounts/register-user.html'
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Account created successfully! Please log in.")
+        return super().form_valid(form)
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/dashboard.html'
